3d8f06b6c9e4a985e878e8310f64124a91c98ce8
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-05-25 17:38:54 +0800

    build: to be able to push to maven, add the Packer class to the module packer

---
+++ packer/src/main/java/io/github/gdrfgdrf/cuteframeworkpacker/Packer.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2024 CuteFramework's Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.gdrfgdrf.cuteframeworkpacker;
+
+/**
+ * 该类仅作占位作用，否则无法上传到 Maven
+ * @author gdrfgdrf
+ * @since v1_0_0_20240525_RELEASE
+ */
+public class Packer {
+}
