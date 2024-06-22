cd1db844927f80a1b89276678b07f8f1de763070
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-06-22 12:23:11 +0800

    build: update to 1.2.3.20240622_Release

--- cute-framework/src/main/java/io/github/gdrfgdrf/cuteframework/common/VersionEnum.java
+++ cute-framework/src/main/java/io/github/gdrfgdrf/cuteframework/common/VersionEnum.java
@@ -91,6 +91,13 @@ public enum VersionEnum {
             "20240622",
             PublishChannel.RELEASE
     ),
+    v1_2_3_20240622_RELEASE(
+            "1",
+            "2",
+            "3",
+            "20240622",
+            PublishChannel.RELEASE
+    ),
 
     /**
      * 不可用的版本，当插件所定义的核心版本在该枚举类中找不到时将返回该类
